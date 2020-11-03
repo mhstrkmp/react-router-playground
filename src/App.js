bb72198799973886d5e9f3455eb4c7ccf7899564
Matthias Heisterkamp <hello@mhstrkmp.dev>
2020-11-03 14:08:11 +0100

    :construction: Add basic components

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Button from './components/Button';
+import WishListItem from './components/WishListItem';
+import GlobalStyle from './GlobalStyle';
 
 function App() {
   return (
-    <>
-      <Router>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Switch>
-          <Route path="/about">About</Route>
-          <Route path="/">Home</Route>
-        </Switch>
-      </Router>
-    </>
+    <Router>
+      <GlobalStyle />
+      <Switch>
+        <Route path="/">
+          <WishListItem title="Matthias" />
+          <Button>+</Button>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
