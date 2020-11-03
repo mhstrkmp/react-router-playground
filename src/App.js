a551e75ab3259e4573f812684a34cd1e810b2db5
Matthias Heisterkamp <hello@mhstrkmp.dev>
2020-11-03 10:25:00 +0100

    :seedling: Initial commit

--- src/App.js
+++ src/App.js
@@ -1,67 +1,26 @@
-import styled, { keyframes } from 'styled-components/macro';
-import GlobalStyle from './GlobalStyle';
-import logo from './logo.svg';
-
-const logoSpin = keyframes`
-  from {
-    transform: rotate(0deg);
-  }
-  to {
-    transform: rotate(360deg);
-  }
-`;
-
-const Wrapper = styled.div`
-  text-align: center;
-
-  header {
-    background-color: #282c34;
-    min-height: 100vh;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    font-size: calc(10px + 2vmin);
-    color: white;
-  }
-
-  header img {
-    height: 40vmin;
-    pointer-events: none;
-  }
-
-  @media (prefers-reduced-motion: no-preference) {
-    header img {
-      animation: ${logoSpin} infinite 20s linear;
-    }
-  }
-`;
-
-const Link = styled.a`
-  color: #61dafb;
-`;
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <GlobalStyle />
-      <Wrapper>
-        <header>
-          <img src={logo} alt="logo" />
-          <p>
-            Edit
-            <code>src/App.js</code>
-            and save to reload.
-          </p>
-          <Link
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </Link>
-        </header>
-      </Wrapper>
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/about">About</Route>
+          <Route path="/">Home</Route>
+        </Switch>
+      </Router>
     </>
   );
 }
